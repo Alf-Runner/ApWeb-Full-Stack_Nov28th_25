ef9f34450b6b60f1f35847071b46a169009a1fbf
Alf-Runner <alfonso.runner.42km@gmail.com>
2025-11-28 23:30:37 -0600

    Create App.vue
    App.vue para el FrontEnd

---
+++ Payment-Frontend/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <PaymentSystem />
+  </div>
+</template>
+
+<script>
+import PaymentSystem from './components/PaymentSystem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PaymentSystem
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background: #f7fafc;
+  color: #2d3748;
+  line-height: 1.6;
+}
+
+#app {
+  min-height: 100vh;
+}
+</style>
